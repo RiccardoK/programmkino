430fd543bf2e156403bdefdc290917eda1dcaeda
RiccardoK <webnoqs@gmail.com>
2019-06-27 03:05:55 +0300

    Film Altersfreigabe und Datum hinzugefügt

--- template_parts/content-filme.php
+++ template_parts/content-filme.php
@@ -8,6 +8,38 @@
 <?php } ?>
 
 
+              <!-- erzeugen variable ($key_fil_start) :  Inhalt des benutz.def Feldes der Variable zuweisen -->
+              <!-- Inhalt des Feldes soll aus aktuellem beitrag kommen daher "get_the_id" : angabe welches ben.def Feld ausgeölesen wird.  -->
+
+            <?php
+              $wpv_film_start = get_post_meta( get_the_ID(), 'wpv_film_start', true );
+              $wpv_film_end = get_post_meta( get_the_ID(), 'wpv_film_end', true );
+              $wpv_film_fsk = get_post_meta( get_the_ID(), 'wpv_film_fsk', true );
+            ?>
+
+            <!--Umformatieren des datums  in format  01.01.2019  -->
+            <?php
+                $wpv_film_start_neu = date('d.m.Y', strtotime($wpv_film_start));
+                $wpv_film_end_neu = date('d.m.Y', strtotime($wpv_film_end));
+            ?>
+
+
+            <!--Ausgabe des Umformatiereten datums  -->
+
+            <?php echo '<p>' . $wpv_film_start_neu . ' - ' . $wpv_film_end_neu . '</p>' ;?>
+
+
+
+            <!--Wenn "Alter" keine Eingabe hat, wird kein Satz ausgegeben -->
+            <?php
+                if ( ! empty( $wpv_film_fsk ) ) {
+                    echo '<p>Geeignet ab ' . $wpv_film_fsk . ' Jahren.</p>' ;
+                }
+            ?>
+
+
+
+
             <!-- //Ausgabe der Taxonomy in "Single-filme.php" -->
             <p class="post-meta">
 
