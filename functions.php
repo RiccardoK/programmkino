55e066f0c8f4ace5911a96f7eb3674b2b66c172c
RiccardoK <webnoqs@gmail.com>
2019-07-11 00:05:39 +0200

    Hinzufügen von Funktionen "Formate" innerhalb des Backend-Editors "TyniMCE" in functions.php

--- functions.php
+++ functions.php
@@ -271,7 +271,15 @@
 
 
 
+            // Custom TinyMCE
 
+            // Callback function to insert 'styleselect' into the $buttons array
+            function my_mce_buttons_2( $buttons ) {
+                array_unshift( $buttons, 'styleselect' );
+                return $buttons;
+            }
+            // Register our callback to the appropriate filter
+            add_filter( 'mce_buttons_2', 'my_mce_buttons_2' );
 
 
 ?>
